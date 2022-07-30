9465656eebf299f3a5508ecc5fee6f666e0f33e1
venkat <venkatasai566@gmail.com>
2022-07-30 13:36:15 -0400

    test

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -3,7 +3,7 @@
 def call (appName, version) {
   withCredentials([usernamePassword(credentialsId: 'admin', passwordVariable: 'pwd', usernameVariable: 'username')]) {
     sh '''
-    docker login -u $username -p $pwd 3.89.93.229:8082
+    docker login -u ${username} -p ${pwd} 3.89.93.229:8082
     docker images -a
     docker push 3.89.93.229:8082/${appName}:${version}
     '''
