7e4371c54561e10dffdd183c0a1a070a829f0d8c
venkat <venkatasai566@gmail.com>
2022-07-30 13:50:38 -0400

    variablized

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -3,6 +3,6 @@
 def call (appName, version) {
 
   withDockerRegistry(credentialsId: 'nexus-admin', url: 'http://3.89.93.229:8082') {
-    sh '''docker push 3.89.93.229:8082/helloworld:v2'''
+    sh '''docker push 3.89.93.229:8082/${appName}:${version}'''
   }
 }
