7f8093d3e47432961ae056807eadc24de4679265
venkat <venkatasai566@gmail.com>
2022-07-30 13:48:32 -0400

    docker push

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,12 +1,8 @@
 /* This file is script to push image to nexus repo */
 
 def call (appName, version) {
-  withCredentials([usernamePassword(credentialsId: 'nexus-admin', passwordVariable: 'pwd', usernameVariable: 'uname')]) {
-    // some block
-    sh '''
-    docker login -u $uname -p $pwd 3.89.93.229:8082
-    docker images -a
-    docker push 3.89.93.229:8082/${appName}:${version}
-    '''
+
+  withDockerRegistry(credentialsId: 'nexus-admin', url: '3.89.93.229:8082') {
+    sh '''docker push 3.89.93.229:8082/helloworld:v2'''
   }
 }
