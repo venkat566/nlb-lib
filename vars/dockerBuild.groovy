951213705df2400a72f30e9b4b5c22d9adb83c72
venkat <venkatasai566@gmail.com>
2022-07-30 13:32:40 -0400

    docker push testing

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -6,7 +6,5 @@ def call (appName, version) {
   docker build -t ${appName}:${version} .
   docker tag ${appName}:${version} 3.89.93.229:8082/${appName}:${version}
   docker images -a
-  docker login -u admin -p admin 3.89.93.229:8082
-  docker push 3.89.93.229:8082/${appName}:${version}
   '''
 }
---
+++ vars/dockerpush.groovy
@@ -0,0 +1,11 @@
+/* This file is script to push image to nexus repo */
+
+def call (appName, version) {
+  withCredentials([usernamePassword(credentialsId: 'admin', passwordVariable: 'pwd', usernameVariable: 'username')]) {
+    sh '''
+    docker login -u $username -p $pwd 3.89.93.229:8082
+    docker images -a
+    docker push 3.89.93.229:8082/${appName}:${version}
+    '''
+  }
+}
