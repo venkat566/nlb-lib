7d6f59591706083b9a9d66e366f26fa6b4c75867
venkat <venkatasai566@gmail.com>
2022-07-30 13:26:50 -0400

    testing push

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -4,6 +4,9 @@ def call (appName, version) {
   echo "---- Docker build -----"
   sh '''
   docker build -t ${appName}:${version} .
-
+  docker tag ${appName}:${version} 3.89.93.229:8082/${appName}:${version}
+  docker images -a
+  docker login -u admin -p admin 3.89.93.229:8082
+  docker push 3.89.93.229:8082/${appName}:${version}
   '''
 }
