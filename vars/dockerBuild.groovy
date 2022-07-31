59101b2b1d66f444a269a84c8f41319ce113f119
venkat <venkatasai566@gmail.com>
2022-07-30 20:53:58 -0400

    checking the docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,11 +3,7 @@
 def call (appName, version, registry, dockerfileName) {
 
   echo "-------------- Docker build image with ${appName} with version ${version} ---------"
-  def cmd = $/
-  docker build -t ${appName}:${version} -f ${dockerfileName} .
-  docker tag ${appName}:${version} ${registry}/${appName}:${version}
-  docker images -a
-  /$
-  shWrapper cmd
-
+  shWrapper 'docker build -t ${appName}:${version} -f ${dockerfileName} .'
+  shWrapper 'docker tag ${appName}:${version} ${registry}/${appName}:${version}'
+  
 }
