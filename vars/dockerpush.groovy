377d07f1e015185f39d7f22427d16c0cf0b8a2fa
venkat <venkatasai566@gmail.com>
2022-07-30 20:49:28 -0400

    fixed registry

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -3,6 +3,6 @@
 def call (appName, version, registry) {
 
   withDockerRegistry(credentialsId: 'nexus-admin', url: 'http://44.202.62.76:8082') {
-    shWrapper 'docker push ${registry}:8082/${appName}:${version}'
+    shWrapper 'docker push ${registry}/${appName}:${version}'
   }
 }
