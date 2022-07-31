a2c6854e5dd98a12c29323c5c97e0c5ede192d22
venkat <venkatasai566@gmail.com>
2022-07-30 20:22:47 -0400

    add docker file name

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,10 +1,10 @@
 /* this is docker build file */
 
-def call (appName, version, registry) {
+def call (appName, version, registry, dockerfileName) {
 
   echo "-------------- Docker build image with ${appName} with version ${version} ---------"
   def cmd = $/
-  docker build -t ${appName}:${version} .
+  docker build -t ${appName}:${version} ${dockerfileName} .
   docker tag ${appName}:${version} ${registry}:8082/${appName}:${version}
   docker images -a
   /$
