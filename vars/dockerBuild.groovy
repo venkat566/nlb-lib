0ab03d38cbeea891fd948e5c8417caba9d05e1b1
venkat <venkatasai566@gmail.com>
2022-07-30 20:45:24 -0400

    fixed command

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -4,7 +4,7 @@ def call (appName, version, registry, dockerfileName) {
 
   echo "-------------- Docker build image with ${appName} with version ${version} ---------"
   def cmd = $/
-  docker build -t ${appName}:${version} ${dockerfileName} .
+  docker build -t ${appName}:${version} -f ${dockerfileName} .
   docker tag ${appName}:${version} ${registry}:8082/${appName}:${version}
   docker images -a
   /$
