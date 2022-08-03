9867cb34f0813fec0cdde6e89c3396ef8319f2bb
venkat <venkatasai566@gmail.com>
2022-08-03 12:31:38 -0400

    Changed the repo url of nexus

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -2,7 +2,7 @@
 
 def call (appName, version, registry) {
 
-  withDockerRegistry(credentialsId: 'nexus-admin', url: 'http://44.202.62.76:8082') {
+  withDockerRegistry(credentialsId: 'nexus-admin', url: 'http://3.82.129.115:8082') {
     shWrapper 'docker push ${registry}/${appName}:${version}'
   }
 }
